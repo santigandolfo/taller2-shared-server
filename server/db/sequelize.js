14983cdd6d7f555f21a510641dd0edc7838d04a5
Christian Angelone <christiangelone@gmail.com>
2017-09-13 12:01:20 -0300

    Changes sequelize to use db URL instead

--- server/db/sequelize.js
+++ server/db/sequelize.js
@@ -1,15 +1,4 @@
-const db_username = process.env.DB_USERNAME || 'postgres';
-const db_password = process.env.DB_PASSWORD || '';
-const db_name = process.env.DB_NAME || 'postgres';
-const db_host = process.env.DB_HOST || 'localhost';
-
+const db_url = process.env.DATABASE_URL;
 const _Sequelize = require('sequelize');
-exports.Sequelize = new _Sequelize(db_name,db_username,db_password, {
-  host: db_host,
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+
+exports.Sequelize = new _Sequelize(db_url);
