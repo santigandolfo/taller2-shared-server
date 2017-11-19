19d7aefbf09356442a20f498f1f9050d32eafa73
Christian Angelone <christiangelone@gmail.com>
2017-11-19 17:55:14 -0300

    Adds Trip model

---
+++ server/models/trip/Trip.js
@@ -0,0 +1,40 @@
+const _Sequelize = require('sequelize');
+const Sequelize = require('../../db/sequelize');
+const sequelize = new Sequelize();
+
+const Trip = sequelize.define('trip', {
+  start_location: {
+    type: Sequelize.GEOMETRY('POINT'),
+    allowNull: false
+  }
+  end_location: {
+    type: Sequelize.GEOMETRY('POINT'),
+    allowNull: false
+  }
+  distance: {
+    type: _Sequelize.DOUBLE,
+    allowNull: false
+  },
+  pay_method: {
+    type: _Sequelize.STRING,
+    allowNull: false
+  }, 
+  currency: {
+    type: _Sequelize.STRING,
+    allowNull: false
+  },
+  cost: {
+    type: _Sequelize.DOUBLE,
+    allowNull: false
+  }, 
+  driver_id: {
+    type: _Sequelize.INTEGER,
+    allowNull: false
+  },
+  passenger_id: {
+    type: _Sequelize.INTEGER,
+    allowNull: false
+  }
+}
+
+module.exports = Trip;
